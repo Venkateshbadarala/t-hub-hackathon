73b315ac2f8d0e1ca0e9e81d4519a041c0533746
MahindraGamini <saimahindra06@gmail.com>
2024-10-01 14:14:50 +0530

    login and signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,14 @@ import Register from './components/Forms/RegisterForm';
 import Login from './components/Forms/LoginForm';
 import ForgotPassword from './components/Forms/ForgotForm';
 import Dashboard from './components/Dashboard/Dashboard';
-import DiaryCard from './components/Dashboard/DiaryCard';
 
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/forgot" element={<ForgotPassword />} />
         <Route path="/dashboard" element={<Dashboard />} />
       
